0c8b8589397dc0c427d32ca10bf46255c70e7f42
Damilare Koiki <koikidamilare@gmail.com>
2023-07-01 22:33:28 +0100

    Change

--- routes/web.php
+++ routes/web.php
@@ -19,16 +19,16 @@ Route::get('/run-migration', function() {
     Artisan::call('migrate');
 });
 
-// Route::get('/', function () {
-//     return view('welcome');
-// });
+Route::get('/', function () {
+    return view('welcome');
+});
 
-// Route::middleware([
-//     'auth:sanctum',
-//     config('jetstream.auth_session'),
-//     'verified'
-// ])->group(function () {
-//     Route::get('/dashboard', function () {
-//         return view('dashboard');
-//     })->name('dashboard');
-// });
+Route::middleware([
+    'auth:sanctum',
+    config('jetstream.auth_session'),
+    'verified'
+])->group(function () {
+    Route::get('/dashboard', function () {
+        return view('dashboard');
+    })->name('dashboard');
+});
