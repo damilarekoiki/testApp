eeeae84411d2d07ef28aa53e105546da4b061530
Damilare Koiki <koikidamilare@gmail.com>
2023-07-01 22:20:13 +0100

    Edit run migration

--- routes/web.php
+++ routes/web.php
@@ -14,21 +14,21 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
-
-Route::middleware([
-    'auth:sanctum',
-    config('jetstream.auth_session'),
-    'verified'
-])->group(function () {
-    Route::get('/dashboard', function () {
-        return view('dashboard');
-    })->name('dashboard');
-});
-
 Route::get('/run-migration', function() {
     Artisan::call('optimize:clear');
     Artisan::call('migrate');
 });
+
+// Route::get('/', function () {
+//     return view('welcome');
+// });
+
+// Route::middleware([
+//     'auth:sanctum',
+//     config('jetstream.auth_session'),
+//     'verified'
+// ])->group(function () {
+//     Route::get('/dashboard', function () {
+//         return view('dashboard');
+//     })->name('dashboard');
+// });
